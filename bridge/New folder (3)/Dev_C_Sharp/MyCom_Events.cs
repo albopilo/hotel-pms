4f13482191a015cc73dc71b89309bf8d7437a4ec
albopilo <albopilosum2+12@gmail.com>
2026-08-23 23:57:13 +0700

    removing large files

--- bridge/New folder (3)/Dev_C_Sharp/MyCom_Events.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Dev_C_Sharp;
-
-[Guid("D11FEA37-AC57-4d39-9522-E49C4F9826BB")]
-[InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
-public interface MyCom_Events
-{
-}
